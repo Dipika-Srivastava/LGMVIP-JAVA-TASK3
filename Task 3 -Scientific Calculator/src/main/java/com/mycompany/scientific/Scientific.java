f17352063adfbfb96165bc8a29bac1999a1f5d3f
Dipika-Srivastava <107299841+Dipika-Srivastava@users.noreply.github.com>
2023-03-22 17:58:25 +0530

    Add files via upload

---
+++ Task 3 -Scientific Calculator/src/main/java/com/mycompany/scientific/Scientific.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.scientific;
+
+/**
+ *
+ * @author Ritika
+ */
+public class Scientific {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
